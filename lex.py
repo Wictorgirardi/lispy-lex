89c3021a3d3ee1c5d799cc76b7cba8ff25befcb0
Wictorgirardi <wictor.girardi@gmail.com>
2021-05-21 19:25:32 -0300

    lex done

--- lex.py
+++ lex.py
@@ -12,5 +12,21 @@ def lex(code: str) -> Iterable[Token]:
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
     da string de código fornecida.
     """
+    tokens = [
+        ("STRING", r"\".*\""),
+        ("NUMBER", r"(\+|\-|)\d+(\.\d*)?"),
+        ("NAME", r"([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*"),
+        ("CHAR", r"#\\[a-zA-Z]*"),
+        ("BOOL", r"#[t|f]"),
+        ("QUOTE", r"\'"),
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+    ]
+
+    treted_code = re.sub(r";;.*", "", code)
+    regex = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
+
+    for token in re.finditer(regex, treted_code):
+        yield Token(token.lastgroup, token.group())
 
     return [Token('INVALIDA', 'valor inválido')]
